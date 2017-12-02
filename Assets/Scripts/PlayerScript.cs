2abcc8242b876dcc4a5c6a920bb015da5a1cbcf6
ajayyy <ajay@ajayinkingston.com>
2017-12-01 23:41:38 -0500

    Added movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    float speed = 500;
+
+    Rigidbody2D body;
+
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+	}
+}
