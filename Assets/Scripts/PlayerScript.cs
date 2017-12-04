7392dd479ed73480330a6a0ff2d0010b81ccdb12
ajayyy <ajay@ajayinkingston.com>
2017-12-03 19:29:41 -0500

    Made the pixel per unit different

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-    float speed = 500;
+    float speed = 1;
+
+    float maxSpeed = 25;
 
     Rigidbody2D body;
 
@@ -13,6 +15,34 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
-	}
+        Vector2 movement = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.D)) {
+            movement += new Vector2(1, 0);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            movement += new Vector2(-1, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+            movement += new Vector2(0, 1);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            movement += new Vector2(0, -1);
+        }
+
+        //body.velocity = movement * speed;
+        body.velocity += movement * speed;
+        CapMaxSpeed();
+        //body.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+    }
+
+    void CapMaxSpeed() {
+        //the max distance they can move in a second is maxSpeed (in a second because everything is multiplied by Time.deltaTime)
+        float distance = Vector2.Distance(body.velocity, Vector2.zero);
+        if (distance >= maxSpeed) {
+            float angle = MathHelper.GetRadians(body.velocity);
+            body.velocity = MathHelper.RadianToVector2(angle) * maxSpeed;
+        }
+    }
 }
