811e4f0e3727abced625b127c849d6df516c0471
ajayyy <ajay@ajayinkingston.com>
2017-12-03 22:18:03 -0500

    can now move and rotate the piece like it's a mini tetris game

--- Assets/Scripts/ShapeScript.cs
+++ Assets/Scripts/ShapeScript.cs
@@ -6,6 +6,13 @@ public class ShapeScript : MonoBehaviour {
 
     Rigidbody2D body;
 
+    public bool attached = false;
+
+    int column;
+    float rotation;
+
+    float lastMove;
+
 	void Start () {
         body = GetComponent<Rigidbody2D>();
 	}
@@ -19,6 +26,30 @@ public class ShapeScript : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.LeftShift)) {
             body.AddForce(MathHelper.RadianToVector2(MathHelper.GetRadians(transform.position - gameController.player.transform.position) - Mathf.PI) * 10);
+        }
+
+        if (attached) {
+            transform.position = gameController.player.transform.position + new Vector3(column, gameController.player.GetComponent<SpriteRenderer>().bounds.size.y);
+
+            transform.localEulerAngles = new Vector3(0, 0, rotation);
+
+            if (Input.GetKey(KeyCode.E) && Time.time - lastMove >= 0.2f) {
+                column++;
+                lastMove = Time.time;
+            }
+
+            if (Input.GetKey(KeyCode.Q) && Time.time - lastMove >= 0.2f) {
+                column--;
+                lastMove = Time.time;
+            }
+
+            if (Input.GetKey(KeyCode.R) && Time.time - lastMove >= 0.2f) {
+                rotation += 90;
+                rotation %= 360;
+                lastMove = Time.time;
+            }
+            print(transform.localEulerAngles);
+
         }
     }
 }
